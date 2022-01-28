fe571c3602e65ce0d057c6a4b1557e286590b938
Ramu Nerella <ramunerella@Ramus-MacBook-Pro.local>
2022-01-28 11:18:59 -0600

    addressing git comments

--- etl/base.py
+++ etl/base.py
@@ -1,73 +1,103 @@
-
 import pandas as pd
 import csv
 import yaml
 import createZipFiles as packaging
+from urllib.parse import urlparse
+import os
 
-with open("config.yml", "rt", encoding='utf8') as ymlfile:
+with open("config.yml", "rt", encoding="utf8") as ymlfile:
     cfg = yaml.safe_load(ymlfile)
 
-df = pd.read_csv(cfg["manifest"]["file_name"], sep='\t', 
-                      
-                      header = 0,
-                      index_col=1,
-                      
-                      usecols = ["file_name", "file_size", "md5sum", "acl", "storage_urls", "series_uid", "study_uid", "case_ids"]
-                      #dtype = {"file_name":str,"file_size":str,"md5sum":str,"acl":str,"storage_urls":object,"series_uid":str,"study_uid":str,"case_ids":str}
-                      )
-# determines which attribute is used for packaging 
-grouped = df.groupby(cfg["manifest"]["package_attribute"])
-output_file = "output/output.tsv"
 
-# create output file to store package contents 
-with open(output_file, 'w') as tsvfile:
-    writer = csv.writer(tsvfile, delimiter="\t", lineterminator="\n")
-    writer.writerow(["record_type"+"    "+"md5"+"   "+"size"+"  "+"authz"+" "+"url"+"   "+"file_name"+" "+"package_contents"])
+def main():
+    dataframe = pd.read_csv(
+        cfg["manifest"]["file_name"],
+        sep="\t",
+        header=0,
+        index_col=1,
+        usecols=[
+            "file_name",
+            "file_size",
+            "md5sum",
+            "acl",
+            "storage_urls",
+            "series_uid",
+            "study_uid",
+            "case_ids",
+        ]
+        # dtype = {"file_name":str,"file_size":str,"md5sum":str,"acl":str,"storage_urls":object,"series_uid":str,"study_uid":str,"case_ids":str}
+    )
+    # output file to store package output
+    output_file = "output/output.tsv"
+    # create output file to store package contents
+    with open(
+        output_file, "a+"
+    ) as tsvfile:  # "a+" means create the file if it does not exist and then open it in append mode
+        writer = csv.writer(tsvfile, delimiter="\t", lineterminator="\n")
+        writer.writerow(
+            [
+                "record_type",
+                "md5",
+                "size",
+                "authz",
+                "url",
+                "file_name",
+                "package_contents",
+            ]
+        )
+    create_outputtsv(dataframe, writer)
+
 
 # parse s3 url to get bucket name and file path
 def split_s3_path(s3_path):
-    path_parts = s3_path.replace("s3://","").split("/")
-    bucket = path_parts.pop(0)
-    file_name = path_parts.pop(len(path_parts) - 1) # remove file_name from url so the whole series_uid folder can be packaged 
-    key = "/".join(path_parts)
-    return bucket, key
+    path_parts = urlparse(s3_path)
+    folder_path = path_parts.path  # contans file path
+    return os.path.dirname(folder_path.lstrip("/"))  # return directory name
+
 
 # to populate package_contents section in the output. package_contents contains details about individual files( like, size, md5sum and file_name)
 def package_contents(group_dataframe):
     package_contents = []
-    for row_index, row in group_dataframe.iterrows():        
+    for row_index, row in group_dataframe.iterrows():
         size = row.get("file_size")
         md5 = row["md5sum"]
         file_name = row["file_name"]
-        row_entry = {"hashes":{"md5sum": md5}, "file_name": file_name, "size": size}
+        row_entry = {"hashes": {"md5sum": md5}, "file_name": file_name, "size": size}
         package_contents.append(row_entry)
     return package_contents
 
-# to populate output tsv files 
-def create_outputtsv():
-    for group_key,group_value in grouped:
-        study_id = df.loc[df["series_uid"] == group_key,"study_uid"].values[0]
-        case_id = df.loc[df["series_uid"] == group_key,"case_ids"].values[0]
+
+# to populate output tsv files
+def create_outputtsv(df, writer):
+    # package_attribute determines which attribute is used for packaging
+    grouped = df.groupby(cfg["manifest"]["package_attribute"])
+    for group_key, group_value in grouped:
+        study_id = df.loc[df["series_uid"] == group_key, "study_uid"].values[0]
+        case_id = df.loc[df["series_uid"] == group_key, "case_ids"].values[0]
         storage_url = df.loc[df["series_uid"] == group_key, "storage_urls"].values[0]
         project_id = df.loc[df["series_uid"] == group_key, "acl"].values[0]
         authz = "/programs/Open/projects/R1"
-        if("A1" in project_id):
+        if "A1" in project_id:
             authz = "/programs/Open/projects/A1"
-        s3_bucket, folder_path = split_s3_path(storage_url)
-        package_name = packaging.createZipFileStream(s3_bucket, folder_path, group_key, "dcm")
-        package_size = packaging.getPackageSize(package_name)
-        package_md5 = packaging.getPackagemd5(package_name)
-        package_url = packaging.getPackageUrl(package_name)
-        writer.writerow(["package"+"    "+(package_md5)+"   "+(package_size)+"  "+(authz)+" "+(package_url)+"   "+case_id+"/"+study_id+"/"+group_key+".zip"
-                            +"  "+str(package_contents(group_value))])
- 
-create_outputtsv()
- 
-   
-      
-    
+        directory_name = split_s3_path(storage_url)
+        package_path = packaging.createZipFileStream(
+            cfg["aws"]["bucket"], directory_name, group_key, "dcm"
+        )
+        package_size = packaging.getPackageSize(package_path)
+        package_md5 = packaging.getPackagemd5(package_path)
+        package_url = packaging.getPackageUrl(package_path)
+        writer.writerow(
+            [
+                "package",
+                (package_md5),
+                (package_size),
+                (authz),
+                (package_url),
+                case_id + "/" + study_id + "/" + group_key + ".zip",
+                str(package_contents(group_value)),
+            ]
+        )
 
-    
-   
 
-
+if __name__ == "__main__":
+    main()
--- etl/createZipFiles.py
+++ etl/createZipFiles.py
@@ -1,69 +1,73 @@
 from io import BytesIO
-import  zipfile, boto3
+import zipfile, boto3
 import botocore
 import yaml
-with open("config.yml", "rt", encoding='utf8') as ymlfile:
+
+
+with open("config.yml", "rt", encoding="utf8") as ymlfile:
     cfg = yaml.safe_load(ymlfile)
 
 AWS_ACCESS_KEY_ID = cfg["aws"]["access_key"]
 AWS_ACCESS_SECRET_ACCESS_KEY = cfg["aws"]["secret_key"]
 AWS_STORAGE_BUCKET_NAME = cfg["aws"]["bucket"]
 
-aws_session = boto3.Session(aws_access_key_id = AWS_ACCESS_KEY_ID,
-                   aws_secret_access_key = AWS_ACCESS_SECRET_ACCESS_KEY)
+aws_session = boto3.Session(
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_ACCESS_SECRET_ACCESS_KEY,
+)
 
 s3 = aws_session.resource("s3")
-s3 = boto3.client("s3", region_name = "us-east-1")
-s3_resource = boto3.resource("s3", aws_access_key_id=AWS_ACCESS_KEY_ID,
-         aws_secret_access_key= AWS_ACCESS_SECRET_ACCESS_KEY)
+s3 = boto3.client("s3", region_name="us-east-1")
+s3_resource = boto3.resource(
+    "s3",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_ACCESS_SECRET_ACCESS_KEY,
+)
+
 
 def getPackageSize(package_name):
     try:
-        package = boto3.resource("s3").Object(AWS_STORAGE_BUCKET_NAME,package_name)
+        package = boto3.resource("s3").Object(AWS_STORAGE_BUCKET_NAME, package_name)
         package_size = package.content_length
     except botocore.exceptions.ClientError:
         package_size = None
         pass
     return package_size
 
+
 def getPackagemd5(package_name):
     try:
-        md5sum = boto3.client('s3').head_object(
-            Bucket=AWS_STORAGE_BUCKET_NAME,
-            Key=package_name
-        )['ETag'][1:-1]
+        md5sum = boto3.client("s3").head_object(
+            Bucket=AWS_STORAGE_BUCKET_NAME, Key=package_name
+        )["ETag"][1:-1]
     except botocore.exceptions.ClientError:
         md5sum = None
         pass
     return md5sum
 
+
 def getPackageUrl(package_name):
-    return "s3://"+AWS_STORAGE_BUCKET_NAME+"/"+package_name
+    return "s3://" + AWS_STORAGE_BUCKET_NAME + "/" + package_name
 
 
 # opens a folder in s3 and package its contents into zip file
-def createZipFileStream(bucketName, bucketFilePath, jobKey, fileExt, createUrl=False):
-    response = {} 
-    bucket = s3_resource.Bucket(bucketName)
-    filesCollection = bucket.objects.filter(Prefix=bucketFilePath).all() 
+def createZipFileStream(bucket_name, directory_name, jobKey, fileExt):
+    bucket = s3_resource.Bucket(bucket_name)
+    filesCollection = bucket.objects.filter(Prefix=directory_name).all()
     archive = BytesIO()
 
-    with zipfile.ZipFile(archive, 'w', zipfile.ZIP_DEFLATED) as zip_archive:
+    with zipfile.ZipFile(archive, "w", zipfile.ZIP_DEFLATED) as zip_archive:
         for file in filesCollection:
-            if file.key.endswith('.' + fileExt):   
-                with zip_archive.open(file.key, 'w') as file1:
-                    file1.write(file.get()['Body'].read())  
+            if file.key.endswith("." + fileExt):
+                with zip_archive.open(file.key, "w") as package_file:
+                    package_file.write(file.get()["Body"].read())
 
-    archive.seek(0)
-    s3_resource.Object(bucketName, bucketFilePath + "/" + jobKey + ".zip").upload_fileobj(archive)
-    package_name = bucketFilePath + "/" + jobKey + ".zip"
+    archive.seek(
+        0
+    )  # moves the pointer to head s its not empty when the zip file created
+    s3_resource.Object(
+        bucket_name, directory_name + "/" + jobKey + ".zip"
+    ).upload_fileobj(archive)
+    package_name = directory_name + "/" + jobKey + ".zip"
     archive.close()
-
-    # response['fileUrl'] = None
-    # if createUrl is True:
-    #     s3Client = boto3.client('s3')
-    #     response['fileUrl'] = s3Client.generate_presigned_url('get_object', Params={'Bucket': bucketName,
-    #                                                         'Key': '' + bucketFilePath + '/' + jobKey + '.zip'}, ExpiresIn=3600)
     return package_name
-
-

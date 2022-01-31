3a22a431fb7bf30d05fc6e5372ad2c70021d95ce
Ramu Nerella <ramunerella@Ramus-MacBook-Pro.local>
2022-01-31 12:46:43 -0600

    updated package md5 logic and resolved some comments

--- etl/base.py
+++ etl/base.py
@@ -4,38 +4,20 @@ import yaml
 import createZipFiles as packaging
 from urllib.parse import urlparse
 import os
-import boto3
 import logging
 
 logging.basicConfig(
-    filename="logs/myapp.log",
     level=logging.DEBUG,
     format="%(asctime)s %(levelname)s %(name)s %(message)s",
 )
+logger = logging.StreamHandler()
 logger = logging.getLogger(__name__)
-
 with open("config.yml", "rt", encoding="utf8") as ymlfile:
     cfg = yaml.safe_load(ymlfile)
 
-AWS_ACCESS_KEY_ID = cfg["aws"]["access_key"]
-AWS_ACCESS_SECRET_ACCESS_KEY = cfg["aws"]["secret_key"]
-AWS_STORAGE_BUCKET_NAME = cfg["aws"]["bucket"]
-
-aws_session = boto3.Session(
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_ACCESS_SECRET_ACCESS_KEY,
-)
-
-s3 = aws_session.resource("s3")
-s3 = boto3.client("s3", region_name="us-east-1")
-s3_resource = boto3.resource(
-    "s3",
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_ACCESS_SECRET_ACCESS_KEY,
-)
-
 
 def main():
+
     dataframe = pd.read_csv(
         cfg["manifest"]["file_name"],
         sep="\t",
@@ -50,15 +32,14 @@ def main():
             "series_uid",
             "study_uid",
             "case_ids",
-        ]
-        # dtype = {"file_name":str,"file_size":str,"md5sum":str,"acl":str,"storage_urls":object,"series_uid":str,"study_uid":str,"case_ids":str}
+        ],
     )
     # output file to store package output
     output_file = "output/output.tsv"
     # create output file to store package contents
     with open(
-        output_file, "a+"
-    ) as tsvfile:  # "a+" means create the file if it does not exist and then open it in append mode
+        output_file, "w"
+    ) as tsvfile:  # "w" open file for writing and reading plain text, create a new file if not exists or truncate the file if exists.
         writer = csv.writer(tsvfile, delimiter="\t", lineterminator="\n")
         writer.writerow(
             [
@@ -98,8 +79,6 @@ def create_outputtsv(df, writer):
     # package_attribute determines which attribute is used for packaging
     grouped = df.groupby(cfg["manifest"]["package_attribute"])
     for group_key, group_value in grouped:
-        study_id = df.loc[df["series_uid"] == group_key, "study_uid"].values[0]
-        case_id = df.loc[df["series_uid"] == group_key, "case_ids"].values[0]
         storage_url = df.loc[df["series_uid"] == group_key, "storage_urls"].values[0]
         project_id = df.loc[df["series_uid"] == group_key, "acl"].values[0]
         authz = "/programs/Open/projects/R1"
@@ -107,30 +86,25 @@ def create_outputtsv(df, writer):
             authz = "/programs/Open/projects/A1"
         try:
             directory_name = split_s3_path(storage_url)
-            package_path = packaging.createZipFileStream(
+            package_name, package_md5 = packaging.createZipFileStream(
                 cfg["aws"]["bucket"], directory_name, group_key, "dcm"
             )
-            package_size = packaging.getPackageSize(package_path)
-            package_md5 = packaging.getPackagemd5(package_path)
-            package_url = packaging.getPackageUrl(package_path)
+            package_size = packaging.getPackageSize(package_name)
+            package_url = packaging.getPackageUrl(package_name)
             writer.writerow(
                 [
                     "package",
-                    (package_md5),
-                    (package_size),
-                    (authz),
-                    (package_url),
-                    case_id + "/" + study_id + "/" + group_key + ".zip",
+                    package_md5,
+                    package_size,
+                    authz,
+                    package_url,
+                    package_name,
                     str(package_contents(group_value)),
                 ]
             )
         except Exception as err:
-            logger.error(
-                "Error occured while processing the study_uid %s and series_uid %s",
-                study_id,
-                group_key,
-            )
             logger.error(err)
+            raise
 
 
 if __name__ == "__main__":
--- etl/createZipFiles.py
+++ etl/createZipFiles.py
@@ -1,41 +1,22 @@
 from io import BytesIO
 import zipfile, boto3
 import botocore
-import yaml
 import hashlib
-from base import s3_resource, AWS_STORAGE_BUCKET_NAME, logger
+from base import logger, cfg
 
-
-with open("config.yml", "rt", encoding="utf8") as ymlfile:
-    cfg = yaml.safe_load(ymlfile)
+AWS_ACCESS_KEY_ID = cfg["aws"]["access_key"]
+AWS_ACCESS_SECRET_ACCESS_KEY = cfg["aws"]["secret_key"]
+AWS_STORAGE_BUCKET_NAME = cfg["aws"]["bucket"]
 
 
 def getPackageSize(package_name):
     try:
         package = boto3.resource("s3").Object(AWS_STORAGE_BUCKET_NAME, package_name)
         package_size = package.content_length
-    except botocore.exceptions.ClientError:
-        package_size = None
-        pass
-    return package_size
-
-
-def getPackagemd5(package_name):
-    try:
-        package = (
-            boto3.client("s3")
-            .object(Bucket=AWS_STORAGE_BUCKET_NAME, Key=package_name)
-            .get()["Body"]
-            .read()
-        )
-        filehash = hashlib.md5()
-        filehash.update(package)
     except botocore.exceptions.ClientError as err:
-        logger.error(
-            "the following occured while processing md5sum for the %s", package_name
-        )
         logger.error(err)
-    return filehash.hexdigest()
+        raise
+    return package_size
 
 
 def getPackageUrl(package_name):
@@ -44,6 +25,11 @@ def getPackageUrl(package_name):
 
 # opens a folder in s3 and package its contents into zip file
 def createZipFileStream(bucket_name, directory_name, jobKey, fileExt):
+    s3_resource = boto3.resource(
+        "s3",
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_ACCESS_SECRET_ACCESS_KEY,
+    )
     bucket = s3_resource.Bucket(bucket_name)
     filesCollection = bucket.objects.filter(Prefix=directory_name).all()
     archive = BytesIO()
@@ -54,12 +40,13 @@ def createZipFileStream(bucket_name, directory_name, jobKey, fileExt):
                 with zip_archive.open(file.key, "w") as package_file:
                     package_file.write(file.get()["Body"].read())
 
-    archive.seek(
-        0
-    )  # moves the pointer to head so its not empty when the zip file created
-    s3_resource.Object(
-        bucket_name, directory_name + "/" + jobKey + ".zip"
-    ).upload_fileobj(archive)
+    # moves the pointer to head so its not empty when the zip file created
+    archive.seek(0)
+    # to calculate md5sum of package
+    filehash = hashlib.md5()
+    filehash.update(open(archive).read())
+    md5 = filehash.hexdigest()
     package_name = directory_name + "/" + jobKey + ".zip"
+    s3_resource.Object(bucket_name, package_name).upload_fileobj(archive)
     archive.close()
-    return package_name
+    return package_name, md5

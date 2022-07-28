2c3eefdda8bfd2860bc6427e472d5fec6ce567eb
Andrew Prokhorenkov <aprokh@uchicago.edu>
2022-07-27 20:39:23 -0500

    feat: arguments instead of config file for running script

--- etl/extractData.py
+++ etl/extractData.py
@@ -1,68 +1,119 @@
-import json
-import pandas as pd
+import argparse
 import csv
-import yaml
 import os
-import logging
-from gen3.auth import Gen3Auth
-from gen3.query import Gen3Query
-from jsonobject import *
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s %(levelname)s %(name)s %(message)s",
+import pandas as pd
+from gen3.auth import Gen3Auth
+from gen3.submission import Gen3Submission
+
+parser = argparse.ArgumentParser(description="N3C Crosswalk Packaging")
+parser.add_argument(
+    "-cf",
+    "--crosswalk-file",
+    action="store",
+    type=str,
+    required=True,
+    help="N3C crosswalk file with Datavant tokens",
 )
-logger = logging.StreamHandler()
-logger = logging.getLogger(__name__)
-with open("config.yml", "rt", encoding="utf8") as ymlfile:
-    cfg = yaml.safe_load(ymlfile)
-COMMONS = "https://{{cfg[n3c][commons]}}/"
+parser.add_argument(
+    "-qa",
+    "--query-attribute",
+    action="store",
+    type=str,
+    required=True,
+    help="Query attribute for N3C crosswalk file",
+)
+parser.add_argument(
+    "--endpoint",
+    action="store",
+    type=str,
+    required=True,
+    help="Data Commons endpoint URL",
+)
+parser.add_argument(
+    "--creds",
+    action="store",
+    type=str,
+    required=True,
+    help="Path to JSON file with credentials",
+)
+
+args = parser.parse_args()
 
 
 def main():
+    crosswalk_file = args.crosswalk_file
+    query_attribute = args.query_attribute
+    endpoint = args.endpoint
+    creds = args.creds
 
     dataframe = pd.read_csv(
-        cfg["n3c"]["file_name"],
+        crosswalk_file,
         sep=",",
         header=0,
         low_memory=False,
-        usecols=[
-            cfg["n3c"]["query_attribute"],
-        ],
+        usecols=[query_attribute],
     )
-    get_guids(dataframe)
+    get_guids(dataframe, endpoint, creds, query_attribute)
 
 
-def get_guids(df):
-    auth = Gen3Auth(COMMONS, refresh_file="credentials.json")
-    query = Gen3Query(auth)
-    grouped = df.groupby(cfg["n3c"]["query_attribute"])
-    query_string = '{\n  datanode(\n    case_ids: ""\n    type: "imaging_data_file"\n    first: 0\n  ) {\n    id\n    case_ids\n    object_id\n  }\n}'
-    for group_key in grouped:
-        query_string["case_ids"] = group_key
-        list = Gen3Query.query(query_string)
-        create_output_file(list)
+def get_guids(df, endpoint, creds, query_attribute):
+    auth = Gen3Auth(endpoint, refresh_file=creds)
+    sub = Gen3Submission(auth)
+    query_string = """query($case_ids: [String]) {
+      datanode(
+        case_ids: $case_ids
+        type: "imaging_data_file"
+        first: 0
+      ) {
+        id
+        case_ids
+        object_id
+      }
+    }"""
+
+    cases_query = """query($submitter_id: [String]) {
+      case(
+        submitter_id: $submitter_id
+        first: 0
+    ) {
+        id
+        submitter_id
+      }
+    }"""
+
+    crosswalk = []
+
+    cases = df[query_attribute].tolist()
+    case_data = sub.query(cases_query, {"submitter_id": cases})
+    case_ids = {v["submitter_id"]: v["id"] for v in case_data["data"]["case"]}
+
+    for case, case_uuid in case_ids.items():
+        r = sub.query(query_string, {"case_ids": case})
+        for i in r["data"]["datanode"]:
+            crosswalk.append(
+                {
+                    "record_id": i["case_ids"][0],
+                    "midrc_image_guid": i["object_id"],
+                    "midrc_case_id": case_uuid,
+                }
+            )
+
+    create_output_file(crosswalk)
 
 
-def create_output_file(list):
+def create_output_file(crosswalk):
     output_file = "output/METADATA.csv"
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
-    with open(
-        output_file, "w"
-    ) as tsvfile:  # "w" open file for writing and reading plain text, create a new file if not exists or truncate the file if exists.
-        writer = csv.writer(tsvfile, delimiter="|", lineterminator="\n", quotechar="&")
-        writer.writerow(
-            [
-                "id",
-                "midrc_image_guid",
-                "midrc_case_submitter_id",
-            ]
+    with open(output_file, "w") as f:
+        fieldnames = ["record_id", "midrc_image_guid", "midrc_case_id"]
+        writer = csv.DictWriter(
+            f, delimiter="|", lineterminator="\n", quotechar="&", fieldnames=fieldnames
         )
-    for entry in list["data"]["datanode"]:
-        writer.writerow(
-            [
-                entry["case_ids"],
-                entry["object_id"],
-                entry["id"],
-            ]
-        )
+        writer.writeheader()
+        for e in crosswalk:
+            writer.writerow(e)
+
+
+if __name__ == "__main__":
+    main()

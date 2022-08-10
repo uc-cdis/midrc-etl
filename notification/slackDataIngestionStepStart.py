3857e47bee46c654543c45d9202ebb25cb0e01fa
Owen Dominguez <osdominguez@uchicago.edu>
2022-08-10 17:26:02 -0500

    Adding data ingestion slack notifications

---
+++ notification/slackDataIngestionStepStart.py
@@ -0,0 +1,24 @@
+import os
+import http.client
+import json
+import sys
+from urllib.parse import urlparse
+
+print("Inside python program")
+
+url = os.environ.get("HOOKURL")
+endpoint = urlparse(url).path
+
+conn = http.client.HTTPSConnection("hooks.slack.com")
+
+payload = {"text": "An Argo data ingestion step has been started."}
+payload = json.dumps(payload)
+
+headers = {"Content-Type": "application/json"}
+
+conn.request("POST", endpoint, payload, headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))

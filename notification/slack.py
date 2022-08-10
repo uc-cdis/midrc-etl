6b809ad4245e698ed7ebdcf093aac2b98328494a
swatkatsrazor <vaitheesh@uchicago.edu>
2022-08-10 16:00:55 -0500

    Create slack.py

---
+++ notification/slack.py
@@ -0,0 +1,22 @@
+import os
+import http.client
+import json
+import sys
+from urllib.parse import urlparse
+
+url = os.environ.get("HOOKURL")
+endpoint = urlparse(url).path
+
+conn = http.client.HTTPSConnection("hooks.slack.com")
+
+payload = {"text": "Hello from file stored at notification-slack-py, initiated from Workflow"}
+payload = json.dumps(payload)
+
+headers = {"Content-Type": "application/json"}
+
+conn.request("POST", endpoint, payload, headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))

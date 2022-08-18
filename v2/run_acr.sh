e7aab63a661b8a5f2a6486fdb516c336cc38df1f
Owen Dominguez <osdominguez@uchicago.edu>
2022-08-18 09:25:18 -0600

    Added TestML Files
    Adding the POC ML Analysis files for data transfer.

--- v2/run_acr.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-python3 process_acr_submission.py \
-    --submission ACR_20220314 \
-    --input_path /Users/andrewprokhorenkov/CTDS/projects/midrc/ssot-s3/replicated-data-acr \
-    --output_path /Users/andrewprokhorenkov/CTDS/projects/midrc/indexing-data/packages_acr
--- v2/run_rsna.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-python3 process_rsna_submission.py \
-    --submission RSNA_20220314 \
-    --input_path /Users/andrewprokhorenkov/CTDS/projects/midrc/ssot-s3/replicated-data-rsna \
-    --output_path /Users/andrewprokhorenkov/CTDS/projects/midrc/indexing-data/packages_rsna
